5f4068830cb57626d1e91b98c02d0670f21af472
Roberto Dijo <robertodijo@gmail.com>
2016-09-04 23:15:06 +0200

    initial

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+  mainHeading = Config.MAIN_HEADING;
+  videos: Array<Video>;
+
+constructor(){
+  this.videos=[
+    new Video(1, "Angular 2 Tut7","f8qBeaGe2S4","Angular 2 for Beginners - Tutorial 7 - Nested Components"),
+    new Video(2, "Angular 2 Tut 5","uq4kLWPfosU","Data Binding and Variables")
+  ]
+}
+}
